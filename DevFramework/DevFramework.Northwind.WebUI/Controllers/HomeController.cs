cd24b416633471e226d46d0125925f7b270d0940
weathered57 <a_canozdogru57@hotmail.com>
2019-10-20 00:48:06 +0300

    WebUI tarafında web.config ayarlanması

---
+++ DevFramework/DevFramework.Northwind.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using DevFramework.Northwind.DataAccess.Concrete.EntityFramework;
+using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevFramework.Northwind.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
