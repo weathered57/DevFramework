d0769dbc6bbba6f09d3768f35e9ba017d8765b00
weathered57 <a_canozdogru57@hotmail.com>
2019-10-20 00:22:31 +0300

    NortwindContext ve EfProductDal oluşturulması

---
+++ DevFramework/DevFramework.Northwind.DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,18 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.EntityFramework
+{
+    public class NorthwindContext : DbContext
+    {
+        public NorthwindContext()
+        {
+            Database.SetInitializer<NorthwindContext>(null);
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
