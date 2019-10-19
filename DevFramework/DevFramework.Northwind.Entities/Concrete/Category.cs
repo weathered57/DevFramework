7374216027b5485ed743551998d34a8507c10501
weathered57 <a_canozdogru57@hotmail.com>
2019-10-20 00:53:36 +0300

    Category sınıfının oluşturması

---
+++ DevFramework/DevFramework.Northwind.Entities/Concrete/Category.cs
@@ -0,0 +1,13 @@
+﻿using DevFramework.Northwind.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFramework.Northwind.Entities.Concrete
+{
+   public class Category : IEntity
+    {
+        public virtual int CategoryId { get; set; }
+        public virtual string CategoryName { get; set; }
+    }
+}
