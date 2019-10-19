8ba68859627de4d1cc523e0d9a93bbe8e61b7542
weathered57 <a_canozdogru57@hotmail.com>
2019-10-20 00:10:43 +0300

    Product sınıfının oluşturulması

---
+++ DevFramework/DevFramework.Northwind.DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,11 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFramework.Northwind.DataAccess.Abstract
+{
+    public interface IProductDal : IEntityRepository<Product>
+    {
+    }
+}
