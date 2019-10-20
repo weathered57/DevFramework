7bdc1177dea41615e5861d3d25a2078323da8264
weathered57 <a_canozdogru57@hotmail.com>
2019-10-20 12:21:34 +0300

    ProductManager ve IProduct oluşturulması

---
+++ DevFramework/DevFramework.Northwind.Business/Abtract/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFramework.Northwind.Business.Abtract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        Product GetById(int id);
+        Product Add(Product product);
+        Product Update(Product product);
+    }
+}
---
+++ DevFramework/DevFramework.Northwind.Business/Concrete/Managers/ProductManager.cs
@@ -0,0 +1,35 @@
+﻿using DevFramework.Northwind.Business.Abtract;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFramework.Northwind.Business.Concrete.Managers
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAll()
+        {
+           return _productDal.GetList();
+        }
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+        }
+        public Product Add(Product product)
+        {
+           return  _productDal.Add(product);
+        }
+        public Product Update(Product product)
+        {
+            return _productDal.Update(product);
+        }
+    }
+}
