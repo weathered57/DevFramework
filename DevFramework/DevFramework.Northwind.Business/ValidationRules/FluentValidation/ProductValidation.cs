9bdd85bf6d18a736ce7a841726b98bfed595cccb
weathered57 <a_canozdogru57@hotmail.com>
2019-10-20 12:03:55 +0300

    FluentValidation eklenmesi

---
+++ DevFramework/DevFramework.Northwind.Business/ValidationRules/FluentValidation/ProductValidation.cs
@@ -0,0 +1,26 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFramework.Northwind.Business.ValidationRules.FluentValidation
+{
+   public class ProductValidation : AbstractValidator<Product>
+    {
+        public ProductValidation()
+        {
+            RuleFor(p => p.ProductId).NotEmpty().WithMessage("Boş Geçilemez");
+            RuleFor(p => p.ProductName).NotEmpty().WithMessage("Boş Geçilemez"); ;
+            RuleFor(p => p.UnitPrice).GreaterThan(0).WithMessage("0 ve ya 0 dan küçük olamaz"); ;
+            RuleFor(p => p.QuantityPerUnit).NotEmpty();
+            RuleFor(p => p.ProductName).Length(2,20);
+            RuleFor(p => p.UnitPrice).GreaterThan(20).When(x => x.CategoryId == 1);
+            //RuleFor(p => p.ProductName).Must(StartWithA);
+        }
+        //private bool StartWithA(string arg)
+        //{
+        //    return arg.StartsWith("A");
+        //}
+    }
+}
